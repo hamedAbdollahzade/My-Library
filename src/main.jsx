8a99ec4941ef019795b13762654b1186beef8854
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-05-21 15:44:17 +0330

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import { Toaster } from "react-hot-toast";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Toaster  toa/>
+    <Toaster  />
   </React.StrictMode>
 );
